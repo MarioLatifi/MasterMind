858b4765691e4a6d0f1bb7125d93c92f755e35ae
Mario Latifi <mario.latifi.stud@ispascalcomandini.it>
2025-02-06 10:24:24 +0100

    aggiunta gitignore

--- MasterMind/obj/Debug/net8.0-windows/MasterMind.AssemblyInfo.cs
+++ MasterMind/obj/Debug/net8.0-windows/MasterMind.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterMind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23af0b84e7d6ff3cc2d5f9cbad50be6e69b5703f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c114c9b631d1857040b34105bd86cb435dbc61c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterMind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterMind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
